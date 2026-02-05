8f1604aa639c1729a7e7069fe9577b36a809e55c
Neil Vallecer <neilvallecer12@gmail.com>
2026-02-05 17:16:10 +0800

    refactor: use D1 appbinding directly and simplify client creation
    - removed the items table and defined the users and accounts tables

--- apps/backend/src/config/db/index.ts
+++ apps/backend/src/config/db/index.ts
@@ -1,20 +1,15 @@
-import { createClient } from '@libsql/client'
-import { drizzle } from 'drizzle-orm/libsql'
-import * as schema from '@/database/schema'
-import { Context } from 'hono'
+import { drizzle } from "drizzle-orm/d1";
+import * as schema from "@/database/schema";
+import { Context } from "hono";
+import { AppBindings } from "@/lib/types/app-types";
 
-export function createDb(c: Context) {
-  if (!c.env.DATABASE_URL)
-    throw new Error('Database URL is required')
+export function createDb(c: Context<AppBindings>) {
+  if (!c.env.DB) throw new Error("Database binding is required");
 
-  const client = createClient({
-    url: c.env.DATABASE_URL,
-    // authToken: env.DATABASE_AUTH_TOKEN,
-  })
-
-  const db = drizzle(client, {
+  // Use the D1 binding directly
+  const db = drizzle(c.env.DB, {
     schema,
-  })
-  return { db, client }
-}
+  });
 
+  return { db };
+}
