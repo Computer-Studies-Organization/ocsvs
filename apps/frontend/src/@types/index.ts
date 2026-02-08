1df0655e6e9766f44151db77c0e65d1bdc3b7269
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-08 20:51:33 +0800

    feat(types): add user-related type definitions for authentication and registration

---
+++ apps/frontend/src/@types/index.ts
@@ -0,0 +1,20 @@
+export type TUser = {
+    studentId: string,
+    accountId: string,
+    firstName: string,
+    lastName: string,
+    yearLevel: string,
+    course: string,
+    email: string,
+    hasVoted: boolean,
+}
+
+export type TRegisterUser = Omit<TUser, "accountId" | "hasVoted"> & {
+    username: string;
+    password: string;
+}
+
+export type TLoginUser = {
+    identifier: string;
+    password: string;
+}
