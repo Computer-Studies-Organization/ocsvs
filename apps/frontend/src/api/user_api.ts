69f9cea9da5c6dc6901c5ab0395008016c8987a1
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-22 16:22:56 +0800

    feat(frontend): enhance user API with logout and user fetching functionalities
    - add logoutUser function for user logout
    - implement authMe function to retrieve current user information
    - create fetchUsers function for paginated user retrieval

--- apps/frontend/src/api/user_api.ts
+++ apps/frontend/src/api/user_api.ts
@@ -11,3 +11,18 @@ export const loginUser = async (data: TLoginUser) => {
     const response = await api.post("/login", data)
     return response.data
 }
+
+export const logoutUser = async () => {
+    const response = await api.post("/logout")
+    return response.data
+}
+
+export const authMe = async () => {
+    const response = await api.get("/me");
+    return response.data
+}
+
+export const fetchUsers = async (page: number = 1, limit: number = 100) => {
+    const response = await api.get(`/users?page=${page}&limit=${limit}`);
+    return response.data
+}
