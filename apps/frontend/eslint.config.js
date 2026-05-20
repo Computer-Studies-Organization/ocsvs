957803c141ad1ad42644ccdc54cc374cd312a96c
Neil Vallecer <neilvallecer12@gmail.com>
2026-05-20 16:46:18 +0800

    chore: fix tsconfig moduleResolution and eslint config

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -2,4 +2,8 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   formatters: true,
+  ignores: ['src/routeTree.gen.ts'],
+  rules: {
+    'test/no-import-node-test': 'off',
+  },
 })
